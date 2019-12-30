7b965445de52bfb1b126a2640eb9ed3cdc4dadbf
Schwitzd <schwitzd@gmail.com>
2019-12-30 13:25:54 +0100

    added Resolve-ShortURL.ps1

---
+++ Internet/Resolve-ShortURL.ps1
@@ -0,0 +1,39 @@
+﻿function Resolve-ShortURL {
+	<#
+	.SYNOPSIS
+		Function to resolve a short URL to the absolute URI.
+		
+	.DESCRIPTION
+		Function to resolve a short URL to the absolute URI.
+		
+	.PARAMETER ShortUrl
+		Specifies the ShortURL
+
+	.EXAMPLE
+		Resolve-ShortURL -ShortUrl http://goo.gl/P5PKq
+
+	.NOTES 
+		Author:		Daniel Schwitzgebel
+		Created:	07/08/2016
+		Modified:	08/08/2016
+		Version:	1.0
+		Changes:  
+	#>
+	
+	[CmdletBinding()]
+	[OutputType([System.String])]
+	param(
+		[String[]]$ShortUrl
+	)
+	
+	Foreach ($URL in $ShortUrl){
+		Try
+		{
+			(Invoke-WebRequest -Uri $URL -MaximumRedirection 0 -ErrorAction Ignore).Headers.Location
+		}
+		Catch
+		{
+			throw 'URL not resolved'
+		}
+	}
+}
