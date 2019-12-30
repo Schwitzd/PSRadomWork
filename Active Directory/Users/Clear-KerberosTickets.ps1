591588720ec75e60f7f1050b91c034f8fc11b1b5
Schwitzd <schwitzd@gmail.com>
2019-12-30 12:06:11 +0100

    added Clear-KerberosTickets.ps1

---
+++ Active Directory/Users/Clear-KerberosTickets.ps1
@@ -0,0 +1,54 @@
+function Clear-KerberosTickets
+{
+  <# 
+  .SYNOPSIS 
+    This function will clear all Kerberos Tickets.
+  
+  .DESCRIPTION 
+    This function will clear all Kerberos Tickets on a remote computer.
+    Admin right are required to run this script!
+  
+  .PARAMETER ComputerName 
+    Target computername.
+
+    .PARAMETER UserName 
+    Target username.
+    
+  .EXAMPLE 
+    Clear-KerberosTickets -ComputerName foo -UserName user1
+  
+  .NOTES
+    Author:    Daniel Schwitzgebel
+    Created:   26/10/2018
+    Modified:  10/12/2019
+    Version:   1.2
+    Changes:   1.1  Add    - Invoke-Command for remote computers
+               1.2  Change - Code style improvement
+  #>
+
+  [OutputType([void])]
+  [CmdletBinding()] 
+  param( 
+    [Parameter(Mandatory)] 
+    [String]
+    $ComputerName,
+
+    [Parameter(Mandatory)] 
+    [String]
+    $Username
+  )
+
+  $getCimInstanceParams = @{
+    ClassName    = Win32_LoggedOnUser
+    ComputerName = $ComputerName
+  }
+
+  $loggedOn = Get-CimInstance @getCimInstanceParams | Where-Object { $_.Antecedent.Name -like $Username }
+  
+  foreach ($sess in $loggedOn)
+  {
+    Invoke-Command -ComputerName $ComputerName -ScriptBlock {	
+      C:\Windows\System32\klist.exe purge -li ('0x{0:X}' -f [int]$sess.Dependent.LogonId)
+    }
+  }
+}
