f520c01080e36a9505f05d8e068279f22a6d8551
Schwitzd <schwitzd@gmail.com>
2019-12-30 13:26:55 +0100

    added Get-WiFiSSIDsPassword.ps1

---
+++ Networking/WiFi/Get-WiFiSSIDsPassword.ps1
@@ -0,0 +1,5 @@
+(netsh wlan show profiles) | Select-String “\:(.+)$” | 
+ForEach-Object { $name = $_.Matches.Groups[1].Value.Trim(); $_ } | 
+ForEach-Object { (netsh wlan show profile name=”$name” key=clear) } | 
+Select-String “Key Content\W+\:(.+)$” | ForEach-Object { $pass = $_.Matches.Groups[1].Value.Trim(); $_ } | 
+ForEach-Object { [PSCustomObject]@{ PROFILE_NAME = $name; PASSWORD = $pass } } | Format-Table -AutoSize
