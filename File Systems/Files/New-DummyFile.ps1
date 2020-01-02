79bb05fa065b772874fa22c77011f8a12451d617
Schwitzd <schwitzd@gmail.com>
2020-01-02 15:04:45 +0100

    added New-DummyFile.ps1

--- File Systems/Files/New-DummyFile.ps1
+++ File Systems/Files/New-DummyFile.ps1
@@ -17,18 +17,16 @@
     New-DummyFile -FilePath C:\Temp\1.dummy -Size 1gb
   
   .NOTES 
-    Author:   Daniel Schwitzgebel
-    Created:  07/08/2014
-    Modified: 27/12/2019
-    Version:  1.3.1
-    Updates:  1.1   Add     - Created Function with Parameters
-              1.2   Add     - Added OutputType
-                    Add     - Added Verbose Parameter
-                    Change  - Converted to Advanced Function
-              1.3   Change  - Error handling with try/catch
-              1.3.1 Change  - Better Exception handling
-
-    To do:      
+    Author:     Daniel Schwitzgebel
+    Created:    07/08/2014
+    Modified:   27/12/2019
+    Version:    1.3.1
+    Changes:    1.1   Add     - Created Function with Parameters
+                1.2   Add     - Added OutputType
+                      Add     - Added Verbose Parameter
+                      Change  - Converted to Advanced Function
+                1.3   Change  - Error handling with try/catch
+                1.3.1 Change  - Better Exception handling    
   #>
 
   [OutputType([void])]
@@ -44,14 +42,13 @@
     [double]
     $Size
   )
- 
-  Write-Verbose "Creating a dummy file with size: $Size"
 
   try
   {
     $objFile = [System.IO.File]::Create($FilePath) 
     $objFile.SetLength($Size) 
     $objFile.Close()
+    Write-Verbose "Creating a dummy file with size: $Size"
   }
   catch [System.IO.IOException]
   {
