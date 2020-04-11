f10415927b4b3a8b66846fe38d9a91974db4d9d5
Schwitzd <schwitzd@gmail.com>
2020-04-11 12:57:31 +0200

    Improved code readability and function help

--- Printers/Get-PrinterDriver.ps1
+++ Printers/Get-PrinterDriver.ps1
@@ -5,20 +5,20 @@ function Get-PrinterDriver
     This function will get the list of printer driver installed.
   
   .DESCRIPTION 
-    This function will get the list of printer driver installed on a local or remote Computer.
-    Admin right are required to run this script!
+    This function will get the list of printer driver installed on a local or remote print server.
   
   .PARAMETER ComputerName 
-    Computer name
+    Specifies the print server name.
     
   .EXAMPLE 
-    Get-PrinterDriver -ComputerName foo
+    PS C:\> Get-PrinterDriver -ComputerName foo
+    This command gets the printer drivers on print server 'foo'.
   
   .NOTES 
     Author:     Daniel Schwitzgebel
     Created:    18/04/2016
-    Modified:   20/11/2019
-    Version:    1.2
+    Modified:   11/04/2020
+    Version:    1.2.1
   #>
 
   [OutputType([System.Management.Automation.PSCustomObject])] 
@@ -29,10 +29,26 @@ function Get-PrinterDriver
     $ComputerName
   )
 
-  Get-CimInstance -ClassName Win32_Printer -Namespace 'root\CIMV2' -ComputerName $ComputerName | ForEach-Object {
-    [PSCustomObject] @{
-      'Printer Name' = $($_.Name)
-      'Driver Name'  = $($_.DriverName)
+  process
+  {
+    $getCimInstanceParam = @{
+      ComputerName = $ComputerName
+      ClassName    = 'Win32_Printer'
+      Namespace    = 'root\CIMV2'
+    }
+
+    try 
+    {
+      Get-CimInstance @getCimInstanceParam | ForEach-Object {
+        [PSCustomObject] @{
+          'Printer Name' = $($_.Name)
+          'Driver Name'  = $($_.DriverName)
+        }
+      }
+    }
+    catch
+    {
+      throw "Failed to get printer driver from $ComputerName"
     }
   }
 }
