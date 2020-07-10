793ecf491b51082a834cdc645123143698496653
Schwitzd <schwitzd@gmail.com>
2020-07-10 11:16:27 +0200

    Addedd function Get-OfficeProtectedDocument

---
+++ Office/Get-OfficeProtectedDocument.ps1
@@ -0,0 +1,106 @@
+function Get-OfficeProtectedDocument
+{
+    <# 
+    .SYNOPSIS 
+        This function will gets the list of Microsoft Office documents protected with password.
+    
+    .DESCRIPTION 
+        This function will gets the list of Microsoft Office documents protected with password.
+        It is possible to choose a specific files or a bunch of folders.
+        The detection is done by reading the firsts byte of the file.
+        Supported Office program: Word, Excel, PowerPoint, Visio, Access, Publisher
+    
+    .PARAMETER File
+        Specifies a single or an array of file. 
+    
+    .PARAMETER Folder
+        Specifies a single or an array of folders. 
+
+    .EXAMPLE 
+        PS C:\> Get-OfficeProtectedDocument -File mydocument.docx
+        This command return if the file mydocument.docx is protected by password.
+
+        PS C:\> Get-OfficeProtectedDocument -Folder c:\temp,c:\documents
+        This command scan the above folders for Office documents protected by password.
+    
+    .NOTES 
+        Author:     Daniel Schwitzgebel
+        Created:    10/07/2020
+        Modified:   10/07/2020
+        Version:    1.0
+
+    .LINK
+  #> 
+
+    [OutputType([System.Management.Automation.PSCustomObject])]
+    [CmdletBinding(DefaultParameterSetName = 'File')]
+    param (
+        [Parameter(ParameterSetName = 'File')]
+        [ValidateNotNullOrEmpty()] 
+        [String[]]
+        $File,
+      
+        [Parameter(ParameterSetName = 'Folder')]
+        [ValidateNotNullOrEmpty()]
+        [String[]]
+        $Folder
+    )
+
+    function Test-ProtectedOfficeFile 
+    {
+        param (
+            [String]
+            $FileName
+        )
+
+        $binary = Get-Content -Path $FileName -AsByteStream | Select-Object -First 2000
+        $string = [System.Text.Encoding]::Default.GetString($binary)
+
+        if ($string -match "E.n.c.r.y.p.t.e.d.P.a.c.k.a.g.e")
+        {
+            return $true
+        }
+        else
+        {
+            return $false
+        }
+    }
+
+    $officeExtensions = @("*.do*", "*.xl*", "*.pp*", "*.po*", "*.accd*", "*.md*", "*.vsd*", "*.pub")
+        
+    if ($psCmdlet.ParameterSetName -eq 'File')
+    {
+        foreach ($f in $File)
+        {
+            if (Test-Path -Path $f)
+            {
+                if ($officeExtensions | Where-Object { [IO.Path]::GetExtension($f) -like $_ })
+                {
+                    $result = Test-ProtectedOfficeFile -FileName $f
+
+                    [PSCustomObject]@{
+                        FileName  = $f
+                        Protected = $result
+                    }
+                }
+            }
+        }
+    }
+    else 
+    {
+        foreach ($f in $Folder)
+        {
+            if (Test-Path -Path $f)
+            {
+                Get-ChildItem -Path $f -Include $officeExtensions -Recurse | ForEach-Object -Process {
+                    $result = Test-ProtectedOfficeFile -FileName $_.FullName
+
+                    [PSCustomObject]@{
+                        FileName  = $_.FullName
+                        Protected = $result
+                    }
+                }
+            }
+        }
+    }
+}
