a8b1d2fe07778cee10cf1323b68bb0864171fae2
Schwitzd <schwitzd@gmail.com>
2020-07-10 14:05:31 +0200

    Release 2

--- Office/Get-ProtectedOfficeDocument.ps1
+++ Office/Get-ProtectedOfficeDocument.ps1
@@ -10,50 +10,41 @@ function Get-ProtectedOfficeDocument
         The detection is done by reading the first 2 Bytes of the file.
         Supported Office programs: Word, Excel, PowerPoint, Visio, Access, Publisher
     
-    .PARAMETER File
-        Specifies a single or an array of file. 
+    .PARAMETER Path
+        Specifies a single or an array of file/folder. 
     
-    .PARAMETER Folder
-        Specifies a single or an array of folders. 
-
     .EXAMPLE 
-        PS C:\> Get-ProtectedOfficeDocument -File mydocument.docx
+        PS C:\> Get-ProtectedOfficeDocument -Path mydocument.docx
         This command return if the file mydocument.docx is protected by password.
 
-        PS C:\> Get-ProtectedOfficeDocument -Folder c:\temp,c:\documents
+        PS C:\> Get-ProtectedOfficeDocument -Path c:\temp,c:\documents
         This command scan the above folders for Office documents protected by password.
     
     .NOTES 
         Author:     Daniel Schwitzgebel
         Created:    10/07/2020
         Modified:   10/07/2020
-        Version:    1.0
+        Version:    2.0
 
     .LINK
   #> 
 
     [OutputType([System.Management.Automation.PSCustomObject])]
-    [CmdletBinding(DefaultParameterSetName = 'File')]
     param (
-        [Parameter(ParameterSetName = 'File')]
+        [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()] 
         [String[]]
-        $File,
-      
-        [Parameter(ParameterSetName = 'Folder')]
-        [ValidateNotNullOrEmpty()]
-        [String[]]
-        $Folder
+        $Path
     )
 
     function Test-ProtectedOfficeFile 
     {
         param (
             [String]
-            $FileName
+            $Path
         )
 
-        $binary = Get-Content -Path $FileName -AsByteStream | Select-Object -First 2000
+        $binary = Get-Content -Path $Path -AsByteStream | Select-Object -First 2000
         $string = [System.Text.Encoding]::Default.GetString($binary)
 
         if ($string -match "E.n.c.r.y.p.t.e.d.P.a.c.k.a.g.e")
@@ -67,40 +58,30 @@ function Get-ProtectedOfficeDocument
     }
 
     $officeExtensions = @("*.do*", "*.xl*", "*.pp*", "*.po*", "*.accd*", "*.md*", "*.vsd*", "*.pub")
-        
-    if ($psCmdlet.ParameterSetName -eq 'File')
-    {
-        foreach ($f in $File)
-        {
-            if (Test-Path -Path $f)
-            {
-                if ($officeExtensions | Where-Object { [IO.Path]::GetExtension($f) -like $_ })
-                {
-                    $result = Test-ProtectedOfficeFile -FileName $f
+    $officeFiles = [System.Collections.ArrayList]@()
 
-                    [PSCustomObject]@{
-                        FileName  = $f
-                        Protected = $result
-                    }
-                }
+    foreach ($p in $Path)
+    {
+        if (Test-Path -LiteralPath $p)
+        {
+            if ((Get-Item -LiteralPath $p) -is [System.IO.DirectoryInfo])
+            {
+                $files = Get-ChildItem -Path $p -Include $officeExtensions -Recurse
+                [void]$officeFiles.AddRange($files)
+            }
+            elseif ((Get-Item -LiteralPath $p) -is [System.IO.FileInfo])
+            {
+                [void]$officeFiles.add($p)
             }
         }
     }
-    else 
-    {
-        foreach ($f in $Folder)
-        {
-            if (Test-Path -Path $f)
-            {
-                Get-ChildItem -Path $f -Include $officeExtensions -Recurse | ForEach-Object -Process {
-                    $result = Test-ProtectedOfficeFile -FileName $_.FullName
 
-                    [PSCustomObject]@{
-                        FileName  = $_.FullName
-                        Protected = $result
-                    }
-                }
-            }
+    $officeFiles | ForEach-Object -Process {
+        $result = Test-ProtectedOfficeFile -Path $_
+
+        [PSCustomObject]@{
+            FileName  = $_
+            Protected = $result
         }
     }
 }
