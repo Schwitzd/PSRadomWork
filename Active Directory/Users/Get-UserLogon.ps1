b85c8a097342c931b82c5cd20d5ced59ca99a45b
Schwitzd <schwitzd@gmail.com>
2020-12-21 13:22:20 +0100

    Get-UserLogon added -Credential Parameter

--- Active Directory/Users/Get-UserLogon.ps1
+++ Active Directory/Users/Get-UserLogon.ps1
@@ -10,6 +10,9 @@ Function Get-UserLogon
   
   .PARAMETER ComputerName 
     Specified the computer name to check the logged in user.
+
+  .PARAMETER Credential
+    Specifies a user account that has permission to perform this action. The default value is the current user.
     
   .EXAMPLE 
     PS C:\> Get-UserLogon -ComputerName foo
@@ -18,8 +21,8 @@ Function Get-UserLogon
   .NOTES 
     Author:    Daniel Schwitzgebel
     Created:   10/03/2018
-    Modified:  09/04/2020
-    Version:   1.2.1
+    Modified:  07/12/2020
+    Version:   1.3.0
   #>
   
   [OutputType([System.Management.Automation.PSCustomObject])] 
@@ -27,41 +30,71 @@ Function Get-UserLogon
     [Parameter(Mandatory)]
     [ValidateNotNullOrEmpty()] 
     [String]
-    $ComputerName
+    $ComputerName,
+
+    [Parameter()]
+    [ValidateNotNull()]
+    [System.Management.Automation.PSCredential]
+    [System.Management.Automation.Credential()]
+    $Credential = [System.Management.Automation.PSCredential]::Empty
   )
   
-  $result = @()
-  if (Test-NetConnection -ComputerName $ComputerName -InformationLevel Quiet)
+  begin
   {
-    Invoke-Command -ComputerName $ComputerName -ScriptBlock { quser } | Select-Object -Skip 1 | ForEach-Object {
-      $a = $_.trim() -replace '\s+', ' ' -replace '>', '' -split '\s'
+    $result = @()
+    $invokeCommandParams = @{
+      ComputerName = $ComputerName
+      ScriptBlock  = { quser }
+    }
+
+    if ($Credential -ne [System.Management.Automation.PSCredential]::Empty)
+    {
+      $invokeCommandParams['Credential'] = $Credential
+    }
+  }
+
+  process
+  {
+    try
+    {
+      Invoke-Command @invokeCommandParams | Select-Object -Skip 1 | ForEach-Object {
+        $a = $_.trim() -replace '\s+', ' ' -replace '>', '' -split '\s'
       
-      if ($a[2] -like '*Disc*')
-      {
-        $array = ([ordered]@{
-            'Username'     = $a[0]
-            'Name'         = (Get-ADUser -Identity $a[0] -Properties DisplayName).DisplayName
-            'Session Name' = 'N/A'
-            'State'        = $a[2]
-            'Idle Time'    = $a[3]
-            'Logon Time'   = $a[4..6] -join ' '
-          })
+        if ($a[2] -like '*Disc*')
+        {
+          $array = ([ordered]@{
+              'Username'     = $a[0]
+              'Name'         = (Get-ADUser -Identity $a[0] -Properties DisplayName).DisplayName
+              'Session Name' = 'N/A'
+              'State'        = $a[2]
+              'Idle Time'    = $a[3]
+              'Logon Time'   = $a[4..6] -join ' '
+            })
  
-        $result += New-Object -TypeName PSCustomObject -Property $array
-      }
-      else
-      {
-        $array = ([ordered]@{
-            'Username'     = $a[0]
-            'Name'         = (Get-ADUser -Identity $a[0] -Properties DisplayName).DisplayName
-            'Session Name' = $a[1]
-            'State'        = $a[3]
-            'Idle Time'    = $a[4]
-            'Logon Time'   = $a[5..7] -join ' ' 
-          })
-        $result += New-Object -TypeName PSCustomObject -Property $array
+          $result += New-Object -TypeName PSCustomObject -Property $array
+        }
+        else
+        {
+          $array = ([ordered]@{
+              'Username'     = $a[0]
+              'Name'         = (Get-ADUser -Identity $a[0] -Properties DisplayName).DisplayName
+              'Session Name' = $a[1]
+              'State'        = $a[3]
+              'Idle Time'    = $a[4]
+              'Logon Time'   = $a[5..7] -join ' ' 
+            })
+          $result += New-Object -TypeName PSCustomObject -Property $array
+        }
       }
     }
-    Write-Output $result
+    catch
+    {
+      throw $_.Exception.Message
+    }
+  }
+
+  end
+  {
+      Write-Output $result
   }
 }
