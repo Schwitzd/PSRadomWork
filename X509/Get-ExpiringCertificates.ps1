c29caf32a84a6a03248290470da2cf309434baea
Schwitzd <schwitzd@gmail.com>
2023-05-02 16:18:41 +0200

    small cosmetic change

--- X509/Get-ExpiringCertificates.ps1
+++ X509/Get-ExpiringCertificates.ps1
@@ -2,23 +2,23 @@ function Get-ExpiringCertificates
 {
     <#
     .SYNOPSIS
-    Retrieves certificates from the LocalMachine\My store on a remote or local computer that will expire within a specified number of days.
+        Retrieves certificates from the LocalMachine\My store on a remote or local computer that will expire within a specified number of days.
 
     .DESCRIPTION
-    This function retrieves certificates from the LocalMachine\My store on a remote or local computer that will expire within a specified number of days.
+        This function retrieves certificates from the LocalMachine\My store on a remote or local computer that will expire within a specified number of days.
 
     .PARAMETER ExpirationDays
-    Specifies the number of days before a certificate expires.
+        Specifies the number of days before a certificate expires.
 
     .PARAMETER ComputerName
-    Specifies the name of the remote computer to retrieve certificates from. Defaults to the local computer.
+        Specifies the name of the remote computer to retrieve certificates from. Defaults to the local computer.
 
     .PARAMETER Credential
-    Specifies a user account with permissions to retrieve certificates from a remote computer.
+        Specifies a user account with permissions to retrieve certificates from a remote computer.
 
     .EXAMPLE
-    PS C:\> Get-ExpiringCertificates -ExpirationDays 60 -ComputerName MyServer -Credential MyDomain\Administrator
-    Returns all certificates that will expire within the next 60 days on the MyServer computer using the MyDomain\Administrator account.
+        PS C:\> Get-ExpiringCertificates -ExpirationDays 60 -ComputerName MyServer -Credential MyDomain\Administrator
+        Returns all certificates that will expire within the next 60 days on the MyServer computer using the MyDomain\Administrator account.
 
     .NOTES
         Author:     Daniel Schwitzgebel
@@ -65,23 +65,24 @@ function Get-ExpiringCertificates
     
     $scriptBlock = {
         param($ExpirationDays)
-        $certStore = New-Object System.Security.Cryptography.X509Certificates.X509Store("My", [System.Security.Cryptography.X509Certificates.StoreLocation]::LocalMachine)
+        $certStore = New-Object System.Security.Cryptography.X509Certificates.X509Store('My', [System.Security.Cryptography.X509Certificates.StoreLocation]::LocalMachine)
         $certStore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadOnly)
-        $certificates = $certStore.Certificates | Where-Object { $_.NotAfter -lt (Get-Date).AddDays($ExpirationDays) }
+        $certificates = $certStore.Certificates  | Where-Object { $_.NotAfter -lt (Get-Date).AddDays($ExpirationDays) }
         $certificates | ForEach-Object {
             [PSCustomObject]@{
-                Subject    = $_.Subject
-                Issuer     = $_.Issuer
-                Thumbprint = $_.Thumbprint
-                NotAfter   = $_.NotAfter
+                Subject      = $_.Subject
+                Issuer       = $_.Issuer
+                Thumbprint   = $_.Thumbprint
+                SerialNumber = $_.SerialNumber
+                NotAfter     = $_.NotAfter
             }
         }
         $certStore.Close()
     }
 
     $invokeCommandParam = @{
-        Session = $session
-        ScriptBlock = $scriptBlock
+        Session      = $session
+        ScriptBlock  = $scriptBlock
         ArgumentList = $ExpirationDays
     }
 
